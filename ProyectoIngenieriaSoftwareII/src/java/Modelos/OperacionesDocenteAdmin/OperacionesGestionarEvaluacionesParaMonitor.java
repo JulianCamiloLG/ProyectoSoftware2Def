6be03d674ac6f7c635b17d437923d1234a298729
Jhoan Sebastian <js.sebas5@gmail.com>
2017-10-30 23:37:21 -0500

    documentacion metodos creados

--- ProyectoIngenieriaSoftwareII/src/java/Modelos/OperacionesDocenteAdmin/OperacionesGestionarEvaluacionesParaMonitor.java
+++ ProyectoIngenieriaSoftwareII/src/java/Modelos/OperacionesDocenteAdmin/OperacionesGestionarEvaluacionesParaMonitor.java
@@ -17,7 +17,12 @@ import java.util.Date;
  */
 public class OperacionesGestionarEvaluacionesParaMonitor
 {
-    
+    /**
+     * metodo para agregar una pregunta a la base de datos
+     * @param p corresponde a un objeto de tipo pregunta para ingreso de preguntas al sistema
+     * @param a corresponde a un objeto de tipo asignatura para ingreso de preguntas al sistema
+     * @return 
+     */   
     public int ingresarPregunta(Pregunta p, Asignatura a)
     {
         CRUDPregunta crudPregunta = new CRUDPregunta();
@@ -25,6 +30,12 @@ public class OperacionesGestionarEvaluacionesParaMonitor
         return result;
     }
     
+    /**
+     * metodo para ingresar respuesta a la base de datos
+     * @param r objeto de tipo respuesta que tiene idRespuesta,descripcionRespuesta y opcionCorrecta
+     * @param p objeto de tipo pregunta que tiene como finalidad proveer idPregunta para la respuesta a ingresar
+     * @return 
+     */
     public int ingresarRespuesta(Respuesta r, Pregunta p)
     {
         CRUDRespuesta crudRespuesta = new CRUDRespuesta();
@@ -32,6 +43,11 @@ public class OperacionesGestionarEvaluacionesParaMonitor
         return result;
     }
     
+    /**
+     * metodo para borrar una pregunta
+     * @param idPregunta es el único identificador de cada pregunta, con el cual se hace el borrado de la pregunta de la base de datos
+     * @return 
+     */
     public int eliminarPregunta(int idPregunta)
     {
         CRUDPregunta crudPregunta = new CRUDPregunta();
@@ -39,6 +55,11 @@ public class OperacionesGestionarEvaluacionesParaMonitor
         return result;
     }
     
+    /**
+     * funcion para eliminar Respuestas del sistema
+     * @param idRespuesta identificador unico de respuesta que permite hacer el borrado de las respuestas de la base de datos
+     * @return 
+     */
     public int eliminarRespuesta(int idRespuesta)
     {
         CRUDRespuesta crudRespuesta = new CRUDRespuesta();
