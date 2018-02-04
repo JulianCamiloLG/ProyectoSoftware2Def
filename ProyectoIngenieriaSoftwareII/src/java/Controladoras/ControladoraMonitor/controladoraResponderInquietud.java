8c6f3fc681a3dcc86b0f7614c003fdc1716c6ddd
Jhoan Sebastian <js.sebas5@gmail.com>
2018-02-04 16:39:27 -0500

    ALgo que funciona

--- ProyectoIngenieriaSoftwareII/src/java/Controladoras/ControladoraMonitor/controladoraResponderInquietud.java
+++ ProyectoIngenieriaSoftwareII/src/java/Controladoras/ControladoraMonitor/controladoraResponderInquietud.java
@@ -6,7 +6,10 @@
 package Controladoras.ControladoraMonitor;
 
 import Modelos.Entidades.Estudiante;
+import Modelos.Entidades.RespuestaInquietud;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,20 +18,21 @@ import org.springframework.web.servlet.ModelAndView;
  *
  * @author SALDARRIAGA VILLADA
  */
+@Controller
 public class controladoraResponderInquietud
 {
-    private JdbcTemplate jdbcTemplate;
-
-   
 
     @RequestMapping(value = "responderInquietud.htm", method = RequestMethod.GET)
-    public ModelAndView form() {
+    public ModelAndView form(HttpServletRequest request)
+    {
         ModelAndView mav = new ModelAndView();
+        String fechaRespuesta = request.getParameter("fechaRespuesta");
+        String horaRespuesta = request.getParameter("horaInicioRespuesta");
+        System.out.println("esto tiene fecha: "+fechaRespuesta);
+        System.out.println("esto tiene hora: "+horaRespuesta);
         mav.setViewName("responderInquietud");
-        mav.addObject("usuarios", new Estudiante());
+        mav.addObject("respuestas", new RespuestaInquietud());
         return mav;
     }
-    
-    
-    
+
 }
